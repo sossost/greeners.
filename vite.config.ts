dfc8f48f5548512b0ac8cf6363b8689a8f28d83f
jang_yoonsu <shagrat8546@gmail.com>
2023-10-05 21:13:40 +0900

    절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+    }),
+    VitePWA({ registerType: "autoUpdate" }),
+  ],
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      { find: "@utils", replacement: path.resolve(__dirname, "src/utils") },
+      { find: "@hooks", replacement: path.resolve(__dirname, "src/hooks") },
+      { find: "@apis", replacement: path.resolve(__dirname, "src/apis") },
+      { find: "@styles", replacement: path.resolve(__dirname, "src/styles") },
+      { find: "@assets", replacement: path.resolve(__dirname, "src/assets") },
+      { find: "@types", replacement: path.resolve(__dirname, "src/types") },
+      {
+        find: "@constants",
+        replacement: path.resolve(__dirname, "src/constants"),
+      },
+      { find: "@store", replacement: path.resolve(__dirname, "src/store") },
+      {
+        find: "@provider",
+        replacement: path.resolve(__dirname, "src/provider"),
+      },
+    ],
+  },
 });
