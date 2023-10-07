9f99d599a101b1d4cfafc62fe78efab02c7b2ff9
jang_yoonsu <shagrat8546@gmail.com>
2023-10-07 22:11:22 +0900

    feat : 바텀 네비게이션

---
+++ src/components/Molecules/BottomNavBar.tsx
@@ -0,0 +1,105 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { IconType } from "react-icons";
+import { GoHomeFill } from "react-icons/go";
+import { LuGoal } from "react-icons/lu";
+import { BsFillCameraFill } from "react-icons/bs";
+import { BsBarChartFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import { colors } from "@constants/colors";
+import { links } from "@constants/links";
+import { mediaQueryScreenAndMinWidth } from "@utils/mediaQuery";
+import { css } from "@emotion/react";
+
+import { Flex } from "@components/Atoms/Flex";
+
+interface Menu {
+  title: string;
+  icon: IconType;
+  link: string;
+}
+
+const MENUES = [
+  {
+    title: "홈",
+    icon: GoHomeFill,
+    link: links.home,
+  },
+  {
+    title: "챌린지",
+    icon: LuGoal,
+    link: links.challenge,
+  },
+  {
+    title: "인증",
+    icon: BsFillCameraFill,
+    link: links.certification,
+  },
+  {
+    title: "오늘의기록",
+    icon: BsBarChartFill,
+    link: links.todayReocords,
+  },
+  {
+    title: "마이페이지",
+    icon: FaUser,
+    link: links.myPage,
+  },
+];
+
+const BottomNavBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  const handleMenuClick = (menu: Menu) => {
+    navigate(menu.link);
+  };
+
+  return (
+    <Flex
+      css={css`
+        height: 60px;
+        position: fixed;
+        bottom: 0;    
+        width: 100%;
+        background-color: ${colors.white};
+        border-top: 1px solid ${colors.lightGray};
+        ${mediaQueryScreenAndMinWidth(1024)`
+          display: none;
+        `}
+        )}
+    `}
+    >
+      {MENUES.map((menu) => {
+        const Icon = menu.icon;
+        return (
+          <Flex.ColumnCenter
+            key={menu.title}
+            onClick={() => handleMenuClick(menu)}
+            css={{
+              gap: 5,
+              padding: 5,
+              flex: 1,
+            }}
+          >
+            <Icon
+              size={25}
+              color={pathname === menu.link ? colors.primary : colors.secondary}
+            />
+            <span
+              css={{
+                fontSize: "12px",
+                color:
+                  pathname === menu.link ? colors.primary : colors.secondary,
+              }}
+            >
+              {menu.title}
+            </span>
+          </Flex.ColumnCenter>
+        );
+      })}
+    </Flex>
+  );
+};
+
+export default BottomNavBar;
