4d980f0e250c81a1fc9e4c705a4f0b1edf41ad6c
jang_yoonsu <shagrat8546@gmail.com>
2023-10-09 16:18:29 +0900

    스토리북 전역 설정

---
+++ .storybook/preview.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import GlobalStyle from "../src/styles/global.style";
+
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <>
+        <GlobalStyle />
+        <Story />
+      </>
+    ),
+  ],
+};
+
+export default preview;
