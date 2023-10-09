79615bb427ac71cb13f96d9a05d5f9dfee1fd223
jang_yoonsu <shagrat8546@gmail.com>
2023-10-09 16:19:34 +0900

    feat : spacing 구현

---
+++ src/components/Atoms/Spacing/index.stories.tsx
@@ -0,0 +1,35 @@
+import { StoryFn, Meta } from "@storybook/react";
+import Spacing from ".";
+
+export default {
+  title: "Atoms/Spacing",
+  component: Spacing,
+  argTypes: {
+    direction: {
+      options: ["vertical", "horizontal"],
+      control: { type: "radio" },
+    },
+    size: {
+      control: { type: "number" },
+    },
+  },
+} as Meta<typeof Spacing>;
+
+const Template: StoryFn<typeof Spacing> = (args) => (
+  <div
+    css={{
+      display: "flex",
+      flexDirection: args.direction === "vertical" ? "column" : "row",
+    }}
+  >
+    <>item1</>
+    <Spacing {...args} />
+    <>item2</>
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  size: 10,
+  direction: "vertical",
+};
