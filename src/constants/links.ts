12cfc4cfd64059b27edb7714a18850497d32fbd6
jang_yoonsu <shagrat8546@gmail.com>
2023-10-10 03:01:25 +0900

    feat : links 상수 구현

---
+++ src/constants/links.ts
@@ -0,0 +1,9 @@
+export const links = {
+  home: "/",
+  challenge: "/challenge",
+  certification: "/certification",
+  todayReocords: "/today-records",
+  myPage: "/my-page",
+  login: "/login",
+  signUp: "/sign-up",
+};
