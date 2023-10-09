5cfb8ba1f1905aa4133dfd4f44d81d39922b24cd
jang_yoonsu <shagrat8546@gmail.com>
2023-10-10 03:01:16 +0900

    feat : color 상수 구현

---
+++ src/constants/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  accent: "#507771",
+  primary: "#66A07D",
+  secondary: "#93BFA9",
+  pastel: "#C6DFDB",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#707070",
+  warn: "#FF0000",
+  lightGray: "#F6F6F6",
+  darkGray: "#333333",
+};
