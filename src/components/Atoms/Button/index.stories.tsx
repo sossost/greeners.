1efdf9a719bfc6b7b1ccf05c4c53be0c8ba78996
jang_yoonsu <shagrat8546@gmail.com>
2023-10-09 16:19:12 +0900

    feat : 버튼 구현

---
+++ src/components/Atoms/Button/index.stories.tsx
@@ -0,0 +1,26 @@
+import { StoryFn, Meta } from "@storybook/react";
+import Button from ".";
+
+export default {
+  title: "Atoms/Button",
+  component: Button,
+  argTypes: {
+    variant: {
+      options: ["primary", "secondary", "textOnly"],
+      control: { type: "radio" },
+    },
+    size: {
+      options: ["small", "medium", "large"],
+      control: { type: "radio" },
+    },
+  },
+} as Meta<typeof Button>;
+
+const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "Button",
+  variant: "primary",
+  size: "medium",
+};
