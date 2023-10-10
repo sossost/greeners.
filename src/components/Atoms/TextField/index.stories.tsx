9ee506f7ed76dde95d4a8a5b9f0093b1a40075d7
jang_yoonsu <shagrat8546@gmail.com>
2023-10-10 15:27:21 +0900

    feat : text filed 구현

---
+++ src/components/Atoms/TextField/index.stories.tsx
@@ -0,0 +1,23 @@
+import { StoryFn, Meta } from "@storybook/react";
+import TextField from ".";
+
+export default {
+  title: "Atoms/TextField",
+  component: TextField,
+  argTypes: {
+    isError: {
+      control: { type: "boolean" },
+    },
+    fontSize: {
+      control: { type: "number" },
+    },
+  },
+} as Meta<typeof TextField>;
+
+const Template: StoryFn<typeof TextField> = (args) => <TextField {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  isError: false,
+  fontSize: 16,
+};
---
+++ src/components/Atoms/TextField/index.tsx
@@ -0,0 +1,43 @@
+import { colors } from "@/constants/colors";
+import { ForwardedRef, InputHTMLAttributes, forwardRef } from "react";
+
+interface TextFieldProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, "size"> {
+  isError?: boolean;
+  fontSize?: number;
+}
+
+const TextField = forwardRef(
+  (
+    { isError, fontSize = 16, ...props }: TextFieldProps,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    return (
+      <input
+        css={{
+          width: "100%",
+          padding: `${fontSize / 2}px ${fontSize - 4}px`,
+          fontSize: fontSize,
+          lineHeight: `${fontSize + 4}px`,
+          fontWeight: 500,
+          border: `1px solid ${isError ? colors.warn : colors.pastel}`,
+          boxShadow: isError ? `0 0 0 2px ${colors.warn}20` : "none",
+          borderRadius: "0.5rem",
+          outline: "none",
+          "&:hover": {
+            border: `1px solid ${colors.primary}`,
+          },
+          "&:focus": {
+            border: `1px solid ${colors.primary}`,
+            boxShadow: `0 0 0 2px ${colors.primary}20`,
+          },
+          transition: "all 0.2s ease-in-out",
+        }}
+        {...props}
+        ref={ref}
+      />
+    );
+  }
+);
+
+export default TextField;
