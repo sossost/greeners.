a950b82a4ee2291a21cd955b33eaa0e69dbcbd17
jang_yoonsu <shagrat8546@gmail.com>
2023-10-14 21:41:58 +0900

    feat : app auth 상태별 분기 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
+import { app } from "./firebaseApp";
 import { BrowserRouter as Router } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import AppRoutes from "./routes";
-import Layout from "./components/Organisms/Layout";
 import GlobalStyle from "./styles/global.style";
+import { Layout } from "./components/Templates";
+import { Loading } from "./components/Molecules";
 
 function App() {
+  const auth = getAuth(app);
+  const [init, setInit] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(!!auth.currentUser);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <Router>
       <GlobalStyle />
       <Layout>
-        <AppRoutes />
+        {init ? <AppRoutes isAuthenticated={isAuthenticated} /> : <Loading />}
       </Layout>
     </Router>
   );
