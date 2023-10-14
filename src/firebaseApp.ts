cebe68b621adb7c601af87cd0896268f9a482f9b
jang_yoonsu <shagrat8546@gmail.com>
2023-10-14 21:40:04 +0900

    feat : firebase app config 추가

---
+++ src/firebaseApp.ts
@@ -0,0 +1,23 @@
+import { initializeApp, FirebaseApp, getApp } from "firebase/app";
+
+export let app: FirebaseApp;
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+try {
+  app = getApp("app");
+} catch (error) {
+  app = initializeApp(firebaseConfig, "app");
+}
+
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
