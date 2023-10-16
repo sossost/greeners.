941e32ae4f77028e43cbd15b57f7442c275a1082
jang_yoonsu <shagrat8546@gmail.com>
2023-10-16 13:08:54 +0900

    feat : auth context provider 구현

---
+++ src/provider/AuthProvider/index.tsx
@@ -0,0 +1,34 @@
+import { app } from "@/firebaseApp";
+import { User, getAuth, onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export const AuthContext = createContext({
+  user: null as User | null,
+});
+
+const AuthContextProvider = ({ children }: AuthProviderProps) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+  }, [auth]);
+
+  return (
+    <AuthContext.Provider value={{ user: currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
